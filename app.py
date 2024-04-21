7b920ad177e83f68b7ff3acd4df2df668a395280
michalomegalul <dog560154@gmail.com>
2024-04-21 19:26:42 +0200

    Refactor Dockerfile and docker-compose.yaml for PostgreSQL setup and it fucking works

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
 from product_manager import create_product
 from product_manager import fetch_products
+from product_manager import create_category
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('products.html')
+     return render_template('product_form.html')
 
 @app.route('/create_product', methods=['POST'])
 def handle_product_creation():
@@ -16,18 +17,28 @@ def handle_product_creation():
     stock_quantity = int(request.form['stock_quantity'])
     category_id = int(request.form['category_id'])  
 
-    result = create_product(name, description, price, stock_quantity, category_id)
-    if result:
-        return 'Product created successfully!'
-    else:
-        return 'Error creating product.'
+    try:
+        result = create_product(name, description, price, stock_quantity, category_id)
+        return 'Product created successfully!'   
+    except Exception as e:
+        return f'Error creating product: {e}' 
+
+@app.route('/create_category', methods=['POST'])
+def handle_category_creation():
+    name = request.form['name']
+    try:
+        result = create_category(name)
+        return 'Category created successfully!'
+    except Exception as e:
+        return f'Error creating category: {e}'
 @app.route('/products')
 def display_products():
     products = fetch_products()
     for product in products:
         if 'image_url' not in product:
-            product['image_url'] = 'party-dog.png' 
+            product['image_url'] = 'party-dog.png'  # Assign default image if not present
     return render_template('products.html', products=products)
 
+
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True, host='0.0.0.0', port=5433) 
--- db_connect.py
+++ db_connect.py
@@ -1,17 +1,20 @@
 import psycopg2
+import logging
 
 conn_params = {
-    'host': '192.168.1.110',
+    'host': 'postgres',  # Service name in docker-compose.yml
     'port': '5432',
     'database': 'eshop_db',
-    'user': 'admin',
+    'user': 'eshop_user',
     'password': 'admin'
 }
+
 
 def connect_db():
     try:
         conn = psycopg2.connect(**conn_params)
+        logging.info("Database connection successful") 
         return conn
     except (Exception, psycopg2.Error) as error:
-        print(f"Error connecting to PostgreSQL: {error}")
+        logging.error(f"Error connecting to PostgreSQL: {error}")
         return None
